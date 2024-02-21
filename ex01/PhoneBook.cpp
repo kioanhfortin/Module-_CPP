a85e0d77ed8af6d999b06d71c6c3aa79f49c9cd3
Kio-anh Fortin <kfortin@c3r8p5.42quebec.com>
2024-02-21 17:18:14 -0500

    ex00 ok

---
+++ ex01/PhoneBook.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 12:40:41 by kfortin           #+#    #+#             */
+/*   Updated: 2024/02/21 17:14:53 by kfortin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    PhoneBook  instance1;
+    PhoneBook  instance2;
+
+    char *s = NULL;
+    
+    std::cin >> s;
+    if (strcmp(s,"ADD"))
+        // std ::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std ::endl;
+    // if (std ::cin == "ADD")
+    return 0;   
+}
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 12:41:02 by kfortin           #+#    #+#             */
+/*   Updated: 2024/02/21 17:13:23 by kfortin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+#include <iostream>
+#include <string.h>
+// # include <stdio.h>
+
+class PhoneBook
+{
+
+public:
+    char **contact;
+
+private:
+    int size_max;
+};
+
+class Contact
+{
+public:
+    
+    
+    
+};
+
+#endif
