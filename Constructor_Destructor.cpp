980d5222f09c106bd392f4b6a5eb16f19f16c863
Kio-anh Fortin <kfortin@c1r7p9.42quebec.com>
2024-02-25 16:17:57 -0500

    Add contact before 8

--- Constructor_Destructor.cpp
+++ Constructor_Destructor.cpp
@@ -1,30 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*   Constructor_Destructor.cpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/18 12:40:41 by kfortin           #+#    #+#             */
-/*   Updated: 2024/02/21 17:14:53 by kfortin          ###   ########.fr       */
+/*   Created: 2024/02/25 11:11:07 by kfortin           #+#    #+#             */
+/*   Updated: 2024/02/25 11:11:39 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-int main(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
-
-    PhoneBook  instance1;
-    PhoneBook  instance2;
-
-    char *s = NULL;
+Contact::Contact(void){
     
-    std::cin >> s;
-    if (strcmp(s,"ADD"))
-        // std ::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std ::endl;
-    // if (std ::cin == "ADD")
-    return 0;   
+    std::cout << "Contact Contructor called" << std::endl;
+
+    return;
+}
+
+Contact::~Contact(void){
+    
+    std::cout << "Contact Destructor called" << std::endl;
+
+    return;
+}
+
+PhoneBook::PhoneBook(void){
+    
+    std::cout << "PhoneBook Contructor called" << std::endl;
+
+    return;
+}
+
+PhoneBook::~PhoneBook(void){
+    
+    std::cout << "PhoneBook Destructor called" << std::endl;
+
+    return;
 }
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,32 +6,36 @@
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 12:41:02 by kfortin           #+#    #+#             */
-/*   Updated: 2024/02/21 17:13:23 by kfortin          ###   ########.fr       */
+/*   Updated: 2024/02/25 15:47:42 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
-#include <iostream>
-#include <string.h>
-// # include <stdio.h>
+# include "Contact.hpp"
+// #include <string.h>
 
 class PhoneBook
 {
 
 public:
-    char **contact;
-
+    PhoneBook(void);
+    ~PhoneBook(void);
+    
+    PhoneBook add_contact(PhoneBook inst1);
+    std::string search_contact(PhoneBook);
+    int nbr_contact;
+    int index[8];
+    Contact contact[8];
+    
 private:
-    int size_max;
+
 };
 
-class Contact
-{
-public:
-    
-    
-    
-};
+void    ft_check_storage(PhoneBook inst1);
+void    ft_print_message(int i);
+PhoneBook    set_index(PhoneBook inst1);
+PhoneBook    ft_insert_contact(PhoneBook inst1, std::string input, int index);
+void ft_erase_contact(PhoneBook inst1, std::string input);
 
 #endif
