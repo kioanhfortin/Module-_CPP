e05894604e59180fd21f6e3efb379ebbd626bbcc
kfortin <kfortin@student.42.fr>
2024-05-26 20:45:45 -0400

    setup ex05

--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -1,38 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Newsed.hpp                                         :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/25 13:41:57 by kfortin           #+#    #+#             */
-/*   Updated: 2024/05/26 20:39:10 by kfortin          ###   ########.fr       */
+/*   Created: 2024/05/26 20:30:38 by kfortin           #+#    #+#             */
+/*   Updated: 2024/05/26 20:44:13 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef NEWSED_H
-# define NEWSED_H
+#ifndef HARL_H
+# define HARL_H
 #include <iostream>
 #include <fstream>
 #include <string>
 
-class newsed
+class Harl
 {
     
 public:
-    newsed(std::string _s1, std::string _s2);
-    ~newsed();
-    
-    void    setString1(std::string new_s1);
-    std::string getString1(std::string _s1);
-    void    setString2(std::string new_s2);
-    std::string getString2(std::string _s2);
-    void ft_copy_file(std::ifstream &fd1, std::ofstream &fd2);
-
+    void complain(std::string level);
+    void get_debug() {};
 private:
-    std::string _s1;
-    std::string _s2;
-    
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
 };
 
 
