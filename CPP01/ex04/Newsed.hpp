75b0468bb1b407de42efd2582c5383df9b148502
kfortin <kfortin@student.42.fr>
2024-05-26 20:23:45 -0400

    ex04 done need to redo for learning purpose

--- CPP01/ex04/Newsed.hpp
+++ CPP01/ex04/Newsed.hpp
@@ -6,7 +6,7 @@
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 13:41:57 by kfortin           #+#    #+#             */
-/*   Updated: 2024/05/26 16:06:44 by kfortin          ###   ########.fr       */
+/*   Updated: 2024/05/26 20:19:27 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ public:
     std::string getString1(std::string _s1);
     void    setString2(std::string new_s2);
     std::string getString2(std::string _s2);
-    void ft_copy_file(std::ifstream fd1, std::ofstream fd2);
+    void ft_copy_file(std::ifstream &fd1, std::ofstream &fd2);
+
 private:
     std::string _s1;
     std::string _s2;
--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,24 +6,31 @@
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 12:18:16 by kfortin           #+#    #+#             */
-/*   Updated: 2024/05/26 16:07:43 by kfortin          ###   ########.fr       */
+/*   Updated: 2024/05/26 20:21:30 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Newsed.hpp"
 
-void newsed::ft_copy_file(std::ifstream fd1, std::ofstream fd2)
+void newsed::ft_copy_file(std::ifstream &fd1, std::ofstream &fd2)
 {
-    (void)fd2;
-    int c;
-    while (fd1 != EOF)
+    std::string line;
+    while(std::getline(fd1, line))
     {
-        if (c == newsed::getString1(newsed::_s1))
-            // go_check_all_str(*c);
-        else
-            fd2 << c;
+        std::string new_line;
+        std::size_t pos = 0;
+        std::size_t prev_pos = 0;
+        while ((pos = line.find(_s1, pos)) != std::string::npos)
+        {
+            new_line.append(line, prev_pos, pos - prev_pos);
+            new_line.append(_s2);
+            pos += _s1.length();
+            prev_pos = pos;
+        }
+        new_line.append(line, prev_pos, std::string::npos);
+
+        fd2 << new_line << '\n';
     }
-    std::cout << fd2 << std::endl;
 }
 
 int main(int argc, char **argv)
@@ -31,20 +38,21 @@ int main(int argc, char **argv)
     if (argc == 4)
     {
         std::ifstream fd1(argv[1]);
-        if (!fd1)
+        if (!fd1.is_open())
         {
             std::perror("File opening fd1 failed");
             return EXIT_FAILURE;
         }
-        std::string str = std::string(argv[1]) + ".replace";
+        std::string str = (std::string)argv[1] + ".replace";
         std::ofstream fd2(str.c_str());
-        if (!fd2)
+        if (!fd2.is_open())
         {
             std::perror("File opening fd2 failed");
             return EXIT_FAILURE;
         }
         newsed sed = newsed(argv[2], argv[3]);
-        ft_copy_file(fd1, fd2);
+        sed.ft_copy_file(fd1, fd2);
+        
         fd1.close();
         fd2.close();
     }
