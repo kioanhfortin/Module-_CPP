0ea7ac9dba7a5aa1e8a6956f827bec928acc5684
kfortin <kfortin@student.42.fr>
2024-06-12 19:08:14 -0400

    ok

--- CPP04/ex02/main.cpp
+++ CPP04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kfortin <kfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 19:03:43 by kfortin           #+#    #+#             */
-/*   Updated: 2024/06/12 18:53:42 by kfortin          ###   ########.fr       */
+/*   Updated: 2024/06/12 19:07:17 by kfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int main()
 {
     Dog* j = new Dog("doggo");
     Cat* i = new Cat("meooo");
+    // Animal k("wrong");
     
     delete(j);
     delete(i);
