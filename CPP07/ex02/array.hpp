2cc1867df12d855b53304b80168571d7274fe8c8
Your Name <you@example.com>
2024-11-22 21:12:30 -0500

    debut ex02

--- CPP07/ex02/array.hpp
+++ CPP07/ex02/array.hpp
@@ -3,27 +3,65 @@
 
 #include <string>
 #include <iostream>
+#include <type_traits>
 
+template <typename T>
 class Array {
 
 public :
-    template <typename T>
-    T();
-    T(int n);
-    T(const T &other);
-    T &operator = (const T &other);
-    ~T();
+    Array() : data(nullptr), size_(0) {}
+
+    Array(size_t n) : size_(n) {
+        if (n < 0)
+            data = new T[size_];
+    }
+
+    Array(const Array &other) : size_(other.size_){
+        data = new T[size_];
+        for (size_t i = 0; i < size_; i ++) {
+            data[i] = other.data[i];
+        }
+    }
+
+    T &operator = (const T &other) {
+        if (this != &other) {
+            delete[] data;
+            size_ = other.size_;
+            data = new T[size_];
+            for (size_t i = 0; i < size_; i ++) {
+                data[i] = other.data[i];
+            }
+        }
+        return *this;
+    }
+    
+    ~Array() {
+        delete[] data;
+    }
+
+
+    size_t size() const { return size_; }
+
+  T &operator [] (size_t index) {
+        if (index > size_)
+            throw InvalidIndex();
+        return data[index];
+    }
+
+    const T &operator [] (size_t index) const {
+        if (index > size_)
+            throw InvalidIndex();
+        return data[index];
+    }
 
     class InvalidIndex : public std::exception {
         public:
             const char *what(void) const throw();
     };
-
-    void size();
     
 private :
-
+    T *data;
+    size_t size_;
 };
 
-
-#endif;
+#endif
